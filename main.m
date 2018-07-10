f56a1c3d98793e24b41ada43824127ce2d2c5f5d
Muhammet Balcilar <muhammetbalcilar@gmail.com>
2018-07-10 02:04:58 +0200

    add result figure

--- main.m
+++ main.m
@@ -17,10 +17,25 @@ for i=1:size(Test,2)
     y=Test(:,i);    
     x_est=XUT(:,i);
     [lhat,ssdist2]=checkperson(Train,x_est,y,LabelTrain);    
-    Predict(i,1)=lhat;    
+    Predict(i,1)=lhat;
+    S(i,:)=ssdist2;
        
     fprintf('%g Recognition rate : %f \n' ,i,mean(Predict(1:i)==LabelTest(1:i)) )
 end
 
+for i=1:size(Test,2)
+    tmp=S(i,:);
+    pscore=tmp(LabelTest(i));
+    tmp(LabelTest(i))=inf;
+    nscore=min(tmp);
+    Score(i,:)=[pscore nscore];
+end
+
+plot(Score(:,2)-Score(:,1))
+hold on;plot([1 size(Test,2)],[0 0],'r','linewidth',3)
+xlabel('Test instance number');
+ylabel('difference of scores');
+legend({'difference between original class and most similar class score','decision border'})
+title('Score differences between ground truth class and most similar class according to algorithm')
 
 
