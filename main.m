3fc5be7f9ee79946fd8ff1db233d3acd42c2d5a8
Muhammet Balcilar <muhammetbalcilar@gmail.com>
2018-07-10 05:06:16 +0200

    add roc curve and confusion matrix

--- main.m
+++ main.m
@@ -41,3 +41,43 @@ legend({'difference between original class and most similar class score','decisi
 title('Score differences between ground truth class and most similar class according to algorithm')
 
 
+%% multi class roc curve
+label=1:size(S,2);
+for i=1:size(S,2)
+    I=LabelTest==i;
+    SS(:,1)=S(:,i);
+    
+    SS(:,2)=min(S(:,(label~=i))')';
+    
+    tt=zeros(size(S,1),2);
+    tt(I)=1;    
+        
+    [tpr,fpr,thresholds] = roc(tt',-SS');
+    
+    TPR(i,:)=tpr{1};
+    FPR(i,:)=fpr{1};
+end
+    
+figure;hold on;
+for i=1:size(S,2)
+    plot(FPR(i,:),TPR(i,:))
+end
+title('Multi Class Roc Curve');
+xlabel('False Positive');
+ylabel('True Positive');
+
+%% confusion matrix
+TT=zeros(size(S));
+HH=zeros(size(S));
+
+for i=1:size(S,1)
+    
+    TT(i,LabelTest(i))=1;
+    HH(i,Predict(i))=1;
+end
+
+[c,cm,ind,per] = confusion(TT',HH');
+figure;imagesc(cm);
+title('Confusion Matrix');
+xlabel('Actual Class');
+ylabel('Prediction');
